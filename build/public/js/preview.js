02051fbccd941bd6d336f8f2081a378ea2d65df4
Simon Kinslow <skinslow@atolga.tmw.local>
2016-05-13 09:55:55 +0100

    Light box bugs fixed
    Retina cleanup of image and center alignment

--- build/public/js/preview.js
+++ build/public/js/preview.js
@@ -3,7 +3,10 @@ var previewMain;
 
 function preview_init()
 {
-	displayList.preview = document.querySelector("#preview");
+	displayList.preview 					= document.querySelector("#preview");
+	displayList.previewImg 				= document.querySelector("#preview img");
+	displayList.previewPreloader 	= document.querySelector("#preview .preview-preloader-dot");
+
 	displayList.preview.addEventListener("click", preview_close, false);
 
 
@@ -21,6 +24,12 @@ function preview_request(event)
 	displayList.wrapper.classList.add("preview-lock");
 	displayList.wrapper.scrollTop += previewMain.scrollSet;
 
+	displayList.previewPreloader.classList.add("preview-preload-tween");
+
+	displayList.previewImg.setAttribute("src", previewMain.url_asset);
+
+	window.scrollTo(0, 0);
+
 	displayList.preview.classList.remove("preview-default");
 
 	trace(previewMain);
@@ -35,12 +44,12 @@ function preview_close(event)
 
 	displayList.wrapper.classList.remove("preview-lock");
 
-	var t = setTimeout(test, 20);
+	var t = setTimeout(scrollReset, 20);
 
 	delete previewMain;
 }
 
-function test()
+function scrollReset()
 {
 	window.scrollTo(0, (previewMain.scrollSet - 100));
 }
@@ -51,4 +60,6 @@ function preview_end(event)
 	displayList.wrapper.removeEventListener("transitionend", preview_end, false);
 
 	displayList.preview.classList.add("preview-default");
+	displayList.previewPreloader.classList.remove("preview-preload-tween");
+	displayList.previewImg.setAttribute("src", "");
 }
